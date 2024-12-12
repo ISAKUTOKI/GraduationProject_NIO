edced583b1eef7815e5ddde7335c20903f533283
ISAKUTOKI <isakutoki@gmail.com>
2024-12-12 13:07:35 +0800

    WIP

---
+++ thejobwanted-weeklynewspaper-of-nio/testFolder/test_script.gd
@@ -0,0 +1,34 @@
+extends Node2D
+#这是一条用来学习的脚本，没有任何实际作用
+
+
+#变量{
+#enum 枚举
+#bool 布尔
+#variable 几乎可以存储任何类型
+#var 几乎可以存储任何类型
+#float 浮点数
+#int 整数
+#const 常量
+#string 字符串
+#}；
+
+#函数{
+#print（）;	在控制台输出括号内文本
+#}；
+
+#强制固定变量类型{
+#变量类型 变量名 : 想要固定为的变量类型 = 值 ;
+#};
+
+func _ready() -> void:
+	
+	print("hello,godot!")
+	
+	pass
+
+
+func _process(delta: float) -> void:
+	
+	
+	pass
