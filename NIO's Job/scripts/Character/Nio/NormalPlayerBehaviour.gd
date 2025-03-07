e67859ee91ceaf43e25e47c6bdd489f43721b498
ISAKUTOKI <isakutoki@gmail.com>
2025-03-07 17:39:24 +0800

    WIP

--- NIO's Job/scripts/Character/Nio/NormalPlayerBehaviour.gd
+++ NIO's Job/scripts/Character/Nio/NormalPlayerBehaviour.gd
@@ -1,24 +1,69 @@
-extends Node
+extends Node2D
 
+@export var group_name: String = "player"
+
+# 移动相关变量
+@export var moveSpeed: int = 100
+var velocity: Vector2 = Vector2.ZERO
 var isMoving: bool = false
 var canMove: bool = true
+var last_direction: String = "down"
 
 
 func _ready() -> void:
-	pass
+	$AnimatedSprite2D.play("F_idle")
+	add_to_group(group_name)
 
 
 func _process(delta: float) -> void:
 	if canMove:
-		_move()
+		_move(delta)
 
 
-func _move() -> void:
+func _move(delta: float) -> void:
+	velocity = Vector2.ZERO #为了在没有输入的时候停止移动
+	
 	if Input.is_action_pressed("move_up"):
-		$AnimatedSprite2D.play("B_walk")
-	elif Input.is_action_pressed("move_down"):
-		$AnimatedSprite2D.play("F_walk")
-	elif Input.is_action_pressed("move_left"):
-		$AnimatedSprite2D.play("L_walk")
-	elif Input.is_action_pressed("move_right"):
-		$AnimatedSprite2D.play("R_walk")
+		velocity.y -= 1
+		last_direction = "up"
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		last_direction = "down"
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+		last_direction = "left"
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		last_direction = "right"
+
+	if velocity == Vector2.ZERO:
+		isMoving = false
+		match last_direction:
+			"up":
+				if $AnimatedSprite2D.animation != "B_idle":
+					$AnimatedSprite2D.play("B_idle")
+			"down":
+				if $AnimatedSprite2D.animation != "F_idle":
+					$AnimatedSprite2D.play("F_idle")
+			"left":
+				if $AnimatedSprite2D.animation != "L_idle":
+					$AnimatedSprite2D.play("L_idle")
+			"right":
+				if $AnimatedSprite2D.animation != "R_idle":
+					$AnimatedSprite2D.play("R_idle")
+	else:
+		isMoving = true
+		velocity = velocity.normalized() * moveSpeed
+		position += velocity * delta
+
+		# 根据移动方向播放动画
+		if abs(velocity.x) > abs(velocity.y):
+			if velocity.x > 0:
+				$AnimatedSprite2D.play("R_walk")
+			else:
+				$AnimatedSprite2D.play("L_walk")
+		else:
+			if velocity.y > 0:
+				$AnimatedSprite2D.play("F_walk")
+			else:
+				$AnimatedSprite2D.play("B_walk")
--- NIO's Job/scripts/SceneObject/KillZone.gd
+++ NIO's Job/scripts/SceneObject/KillZone.gd
@@ -1,7 +1,19 @@
-extends Node
+extends Area2D
 
+@export var target_group: String = "player"
 
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+# 当有对象进入区域时调用
 func _on_body_entered(body: Node2D) -> void:
-	if body != null:  #检测玩家，如果进入就通知
-		print_debug("玩家进入KillZone")
-		#body.queue_free()
+	# 检查对象是否属于目标组
+	if body.is_in_group(target_group):
+		_kill_or_reset(body)
+
+# 杀死或重置对象
+func _kill_or_reset(body: Node2D) -> void:
+	if body.has_method("kill"):  # 如果对象有 kill 方法，调用它
+		body.kill()
+	else:
+		print("KillZone: 对象无法被杀死")
