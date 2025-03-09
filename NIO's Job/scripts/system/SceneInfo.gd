ba594a7753247404e6f69a59e4d85bb2d157802f
ISAKUTOKI <isakutoki@gmail.com>
2025-03-09 17:12:43 +0800

    WIP

---
+++ NIO's Job/scripts/system/SceneInfo.gd
@@ -0,0 +1,4 @@
+class_name SceneInfo
+extends Node
+
+var 
--- NIO's Job/scripts/system/SceneManager.gd
+++ NIO's Job/scripts/system/SceneManager.gd
@@ -1,5 +1,13 @@
 class_name SceneManager
 extends Node
 
+
+
 func reload_current_scene():
+	pass
+
+func load_next_scene():
+	pass
+
+func load_last_scene():
 	pass
