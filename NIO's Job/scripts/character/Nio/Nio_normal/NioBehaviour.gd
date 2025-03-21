20d5f93678288ba6ab8eb3548b27a8c01782d876
ISAKUTOKI <isakutoki@gmail.com>
2025-03-21 09:31:15 +0800

    WIP

--- NIO's Job/scripts/character/Nio/Nio_normal/NioBehaviour.gd
+++ NIO's Job/scripts/character/Nio/Nio_normal/NioBehaviour.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var group_name: String = "player"
+@export var is_in_test: bool = true
 
 
 func _ready() -> void:
@@ -11,4 +12,4 @@ func kill() -> void:
 	print("Nio没了！")
 	GlobalSignalBus.player_is_dead.emit()
 	queue_free()
-	
+	SceneManager.reload_current_scene(1.5)
--- NIO's Job/scripts/system/SceneManager.gd
+++ NIO's Job/scripts/system/SceneManager.gd
@@ -1,15 +1,18 @@
-class_name SceneManager
 extends Node
 
 
-
-func reload_current_scene():
+func reload_current_scene(delay: float = 0):
+	var current_scene_path = get_tree().current_scene.scene_file_path
+	await get_tree().create_timer(delay).timeout
+	get_tree().change_scene_to_file(current_scene_path)
 	print("重加载了当前场景")
 	pass
+
 
 func load_next_scene():
 	print("加载了下一个场景")
 	pass
+
 
 func load_last_scene():
 	print("加载了上一个场景")
